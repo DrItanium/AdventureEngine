cde5c23ab234b153131048e3a2402f70ff4a2855
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-11 12:42:46 -0700

    Added the another-world folder in the native engine
    This is done as a jumping off point so that I don't get bogged down in
    technical details with interacting with the SDL.

---
+++ src/native/another-world/AnotherWorldNative.h
@@ -0,0 +1,24 @@
+/*
+Another World Expert System Virtual Machine Implementation
+Copyright (C) 2013 Joshua Scoggins 
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+#ifndef _ANOTHER_WORLD_NATIVE_H
+#define _ANOTHER_WORLD_NATIVE_H
+
+extern void AnotherWorldNativeFunctionsDeclaration(void* theEnv);
+
+#endif
